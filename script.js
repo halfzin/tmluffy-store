0afd938d96aa19e88c22ec1fc971f2e9e1cca6e5
TMluffy <halifllima@gmail.com>
2026-01-25 19:09:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,16 @@ function comprar(produto, preco) {
   const whatsapp = "5581987083004";
 
   const mensagem =
-    `Olá! Vim pela TM Luffy Store 👒🍎\n\n` +
-    `🛒 Produto: ${produto}\n` +
-    `💰 Preço: ${preco}\n\n` +
-    `Gostaria de realizar a compra.`;
+    "Olá! Vim pela TM Luffy Store 👒🍎\n\n" +
+    "🛒 Produto: " + produto + "\n" +
+    "💰 Preço: " + preco + "\n\n" +
+    "Gostaria de realizar a compra.";
 
   const link =
-    `https://wa.me/${whatsapp}?text=${encodeURIComponent(mensagem)}`;
+    "https://api.whatsapp.com/send?phone=" +
+    whatsapp +
+    "&text=" +
+    encodeURIComponent(mensagem);
 
   window.open(link, "_blank");
-}
-
-function filtrar() {
-  let texto = document.getElementById("search").value.toLowerCase();
-  let cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-    card.style.display = card.innerText.toLowerCase().includes(texto)
-      ? "block"
-      : "none";
-  });
 }
